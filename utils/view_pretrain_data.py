90328d33a4967fa6bd213e9f9820d1ff06f82751
Subrat <133409319+NoTypo@users.noreply.github.com>
2024-06-08 01:09:58 +0000

    Add finetune data viewer

--- utils/view_pretrain_data.py
+++ utils/view_pretrain_data.py
@@ -1,7 +1,10 @@
 from datasets import load_dataset
 
-pretrain_data_meta = load_dataset("liuhaotian/LLaVA-Pretrain",data_files={'train':'blip_laion_cc_sbu_558k_meta.json'})
-pretrain_data = load_dataset("liuhaotian/LLaVA-Pretrain",data_files={'train':'blip_laion_cc_sbu_558k.json'})
+pretrain_data_meta = load_dataset("liuhaotian/LLaVA-Pretrain", data_files={'train':'blip_laion_cc_sbu_558k_meta.json'})
+pretrain_data = load_dataset("liuhaotian/LLaVA-Pretrain", data_files={'train':'blip_laion_cc_sbu_558k.json'})
 
 print(pretrain_data['train'][1])
 print(pretrain_data_meta['train'][1])
+
+finetuning_data = load_dataset("liuhaotian/LLaVA-Instruct-150K", data_files= {'train':'llava_instruct_150k.json'})
+print(finetuning_data['train'][1])
